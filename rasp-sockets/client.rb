04733a5807c03c36ba212fdb3ef1b991a0d868da
fanig01 <fanig_01@hotmail.com>
2016-10-21 16:30:51 -0200

    done client socket

--- rasp-sockets/client.rb
+++ rasp-sockets/client.rb
@@ -1,3 +1,42 @@
+require 'socket'
+hostname = 'localhost'
+port = 2000
+socket = TCPSocket.new(hostname, port)
+
+begin
+	nome_arquivo = gets.chomp
+	arquivo = File.new(nome_arquivo, "r")
+	tamanho = File::size(nome_arquivo)
+	bytes_arquivo = arquivo.read(tamanho)
+	socket.puts(nome_arquivo)
+	socket.puts(tamanho.to_s)
+	socket.syswrite(bytes_arquivo)
+ensure
+
+	socket.close
+	arquivo.close
+end
+
+
+
+
+
+
+=begin
+socket.puts('soma 5 3')
+puts socket.gets
+
+socket.puts('raiz_quadrada 100')
+puts socket.gets
+
+socket.puts('maluco 5 3')
+puts socket.gets
+socket.close
+=end
+
+
+
+=begin
 # In another script, start this second
 require 'socket'
 include Socket::Constants
@@ -7,3 +46,4 @@ socket.connect(sockaddr)
 socket.puts "Hello from script 2."
 puts "The server said, '#{socket.readline.chomp}'"
 socket.close
+=end
