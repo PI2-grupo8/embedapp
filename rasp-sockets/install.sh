1b690a7ce2ce8b3e5c013a6ccc3d146ee78260f0
Tallys Martins <tallysmartins@gmail.com>
2016-12-05 20:04:08 +0100

    Fixing install script
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- rasp-sockets/install.sh
+++ rasp-sockets/install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+	echo "Installing dependencies"
+  apt-get install iptables
   sudo apt-get install curl -y
   curl -sSL https://rvm.io/mpapis.asc | gpg --import -
   \curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
-  source /home/$USER/.rvm/scripts/rvm
+
+	echo "Sourcing RVM: "
+  source ~/.rvm/scripts/rvm
+
+	echo  "Installing Bundler"
   gem install bundler
+
+	echo  "Installing FTPD"
   gem install ftpd
-  apt-get install iptables
-	echo "========="
-	echo $0
+
+	echo  "Adding services to initd"
   mkdir -p /usr/share/tortuga
 	cp tortuga-server.rb /usr/share/tortuga/
   cp tortuga-server.service /lib/systemd/system/
   cp tortuga-server /etc/init.d/
-  source ~/.rvm/scripts/rvm
 	
